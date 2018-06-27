99e6bf88f0b2e0256f8132d9503e02933659f651
asalekin <asalekin@gmail.com>
2018-06-27 14:11:39 -0400

    my committ 3

--- README.md
+++ README.md
@@ -1,19 +1,6 @@
-## Ppt Controller
-Control MS ppt presentation
+# Hello World Skill
 
-## Description
-Use voice to open, close, advance slides (next slide, prev slide, last slide) from a user's folder
-
-## Examples
- * "open presentation"
- * "next slide"
- * "previous slide"
- * "first slide"
- * "last slide"
- * "close presentation"
- * "open file"
- * "close file"
-
-## Credits
-Avinash Vyas
-
+## Usage:
+* `hello world`
+* `how are you`
+* `thank you`
--- __init__.py
+++ __init__.py
@@ -1,23 +1,59 @@
-import requests
+# Copyright 2016 Mycroft AI, Inc.
+#
+# This file is part of Mycroft Core.
+#
+# Mycroft Core is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Mycroft Core is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Mycroft Core.  If not, see <http://www.gnu.org/licenses/>.
+
 from adapt.intent import IntentBuilder
-from mycroft import MycroftSkill, intent_handler
+
+from mycroft.skills.core import MycroftSkill
 from mycroft.util.log import getLogger
+
+__author__ = 'asalekin'
+
+LOGGER = getLogger(__name__)
 
 
 class RobotGoSkill(MycroftSkill):
     def __init__(self):
         super(RobotGoSkill, self).__init__(name="RobotGoSkill")
 
-    @intent_handler(IntentBuilder("RobotIntent").require('send'))
-    def handle_robot_controller(self, message):
-        print(message)
+    def initialize(self):
+        robot_go_intent = IntentBuilder("RobotGoIntent").require("RobotGoKeyword").require("Word").build()
+        self.register_intent(robot_go_intent, self.handle_robot_go_intent)
 
-    @intent_handler(IntentBuilder("OpenPPTIntent").require('OpenPPT').require("Filename"))
-    def handle_ppt_open(self, message):
-        filename = message.data.get("Filename")
-        print(filename)
+        robot_back_intent = IntentBuilder("RobotBackIntent").require("RobotBackKeyword").require("Word").build()
+        self.register_intent(robot_back_intent, self.handle_robot_back_intent)
+
+
+    def handle_robot_go_intent(self, message):
+        toplaceword = message.data.get("Word")
+        
+        self.speak_dialog("move")
+        self.speak(toplaceword)
+
+    def handle_robot_back_intent(self, message):
+        fromplaceword = message.data.get("Word")
+        
+        self.speak_dialog("back")
+        self.speak(fromplaceword)
+
+
+
+    def stop(self):
+        pass
 
 
 def create_skill():
     return RobotGoSkill()
-
