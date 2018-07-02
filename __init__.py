fb7cdb85afc53a56267ac670bd173b6a062000b9
asalekin <asalekin@gmail.com>
2018-07-02 11:22:55 -0400

    my committ 10a

--- __init__.py
+++ __init__.py
@@ -43,7 +43,7 @@ class RobotGoSkill(MycroftSkill):
         self.speak_dialog("move")
         self.speak(toplaceword)
 
-        print(json.dumps(message, indent=4))
+        print(message)
 
     def handle_robot_back_intent(self, message):
         fromplaceword = message.data.get("Word")
