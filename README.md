5e002596edcd3e3fafc00d92022f149ac0539c63
asalekin <asalekin@gmail.com>
2018-07-31 17:25:40 -0400

    test dialog 1

--- README.md
+++ README.md
@@ -1,25 +1,19 @@
-## Jokes
-Let Mycroft brighten your day with a little humor
+## Ppt Controller
+Control MS ppt presentation
 
-## Description 
-Brighten your day with a little humor.  This draws on the jokes collected by the [PyJokes project](https://github.com/pyjokes/pyjokes) to give you a chuckle.
+## Description
+Use voice to open, close, advance slides (next slide, prev slide, last slide) from a user's folder
 
-The joke categories are:
-* Neutral -- jokes that are safe for work, kids or your grandmother
-* Adult -- nothing horrible, but be ready to cover some ears
-* Chuck Norris -- jokes only a geek can love
+## Examples
+ * "open presentation"
+ * "next slide"
+ * "previous slide"
+ * "first slide"
+ * "last slide"
+ * "close presentation"
+ * "open file"
+ * "close file"
 
-By default it will give you clean and/or geeky jokes, but you can ask a little adult humor if you feel that way.  
+## Credits
+Avinash Vyas
 
-_WARNING:  Laughter is not guaranteed, but eye rolls are likely._
-
-## Examples 
-* "Make me laugh"
-* "Tell me a Chuck Norris joke"
-* "I want to hear a raunchy joke"
-* "How about a neutral joke"
-
-## Credits 
-Mycroft AI
-
-Everyone at https://github.com/pyjokes/pyjokes!
--- __init__.py
+++ __init__.py
@@ -1,59 +1,47 @@
-# Copyright 2017, Mycroft AI Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-from os.path import dirname, join
-
-import pyjokes
-
 from adapt.intent import IntentBuilder
-from mycroft.skills.core import MycroftSkill, intent_handler
-from random import choice
+from mycroft import MycroftSkill, intent_handler
 
 
-joke_types = ['chuck', 'neutral']
 
-
-class JokingSkill(MycroftSkill):
+class PptControllerSkill(MycroftSkill):
     def __init__(self):
-        super(JokingSkill, self).__init__(name="JokingSkill")
+        super(PptControllerSkill, self).__init__(name="PptControllerSkill")
+        self.file_opened = False
 
-    def speak_joke(self, lang, category):
-        self.speak(pyjokes.get_joke(language=lang, category=category))
+    @intent_handler(IntentBuilder("PPTIntent").require('PptController'))
+    def handle_ppt_controller(self, message):
+        self.speak_dialog('ppt.controller')
 
-    @intent_handler(IntentBuilder("JokingIntent").require("Joke"))
-    def handle_general_joke(self, message):
-        selected = choice(joke_types)
-        self.speak_joke(self.lang[:-3], selected)
+    @intent_handler(IntentBuilder("OpenPPTIntent").require('OpenPPT').require("Filename"))
+    def handle_ppt_open(self, message):
+        filename = message.data.get("Filename")
+        self.file_opened = True
+        # Send a rest request
+        param = {'filename':filename}
+        resp = {'filename' : filename}
+        self.speak_dialog('ppt.open', data=resp)
 
-    @intent_handler(IntentBuilder("ChuckJokeIntent").require("Joke")
-                    .require("Chuck"))
-    def handle_chuck_joke(self, message):
-        self.speak_joke(self.lang[:-3], 'chuck')
+    @intent_handler(IntentBuilder("NextSlideIntent").require('NextSlide'))
+    def handle_next_slide(self, message):
+        if self.file_opened:
+            self.speak_dialog('ppt.next')
+        else:
+            self.speak_dialog('ppt.filenotopen')
 
-    @intent_handler(IntentBuilder("NeutralJokeIntent").require("Joke")
-                    .require("Neutral"))
-    def handle_neutral_joke(self, message):
-        self.speak_joke(self.lang[:-3], 'neutral')
+    @intent_handler(IntentBuilder("PrevSlideIntent").require('PrevSlide'))
+    def handle_prev_slide(self, message):
+        if self.file_opened:
+            self.speak_dialog('ppt.prev')
+        else:
+            self.speak_dialog('ppt.filenotopen')
 
-    @intent_handler(IntentBuilder("AdultJokeIntent").require("Joke")
-                    .require("Adult"))
-    def handle_adult_joke(self, message):
-        self.speak_joke(self.lang[:-3], 'adult')
+    @intent_handler(IntentBuilder("ClosePPTIntent").require('ClosePPT'))
+    def handle_ppt_close(self, message):
+        if self.file_opened:
+            self.speak_dialog('ppt.close')
+        else:
+            self.speak_dialog('ppt.filenotopen')
 
-    def stop(self):
-        pass
+    def create_skill():
+        return PptControllerSkill()
 
-
-def create_skill():
-    return JokingSkill()
