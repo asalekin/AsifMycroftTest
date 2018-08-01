d7152d17bba6ebcfce89a87090e9485275db4117
asalekin <asalekin@gmail.com>
2018-08-01 09:50:45 -0400

    test dialog 1

--- __init__.py
+++ __init__.py
@@ -1,47 +1,52 @@
-from adapt.intent import IntentBuilder
-from mycroft import MycroftSkill, intent_handler
+from mycroft.skills.core import FallbackSkill
 
 
-
-class PptControllerSkill(MycroftSkill):
+class MeaningFallback(FallbackSkill):
+    """
+        A Fallback skill to answer the question about the
+        meaning of life, the universe and everything.
+    """
     def __init__(self):
-        super(PptControllerSkill, self).__init__(name="PptControllerSkill")
-        self.file_opened = False
+        super(MeaningFallback, self).__init__(name='Meaning Fallback')
 
-    @intent_handler(IntentBuilder("PPTIntent").require('PptController'))
-    def handle_ppt_controller(self, message):
-        self.speak_dialog('ppt.controller')
+    def initialize(self):
+        """
+            Registers the fallback skill
+        """
+        self.register_fallback(self.handle_fallback, 1)
+        # Any other initialize code goes here
 
-    @intent_handler(IntentBuilder("OpenPPTIntent").require('OpenPPT').require("Filename"))
-    def handle_ppt_open(self, message):
-        filename = message.data.get("Filename")
-        self.file_opened = True
-        # Send a rest request
-        param = {'filename':filename}
-        resp = {'filename' : filename}
-        self.speak_dialog('ppt.open', data=resp)
+    def handle_fallback(self, message):
+        """
+            Answers question about the meaning of life, the universe
+            and everything.
+        """
+        utterance = message.data.get("utterance")
 
-    @intent_handler(IntentBuilder("NextSlideIntent").require('NextSlide'))
-    def handle_next_slide(self, message):
-        if self.file_opened:
-            self.speak_dialog('ppt.next')
+        # get keywords for current language
+        what = self.dialog_renderer.render('query')
+        meaning = self.dialog_renderer.render('meaning')
+        life = self.dialog_renderer.render('life')
+        universe = self.dialog_renderer.render('universe')
+        everything = self.dialog_renderer.render('everything')
+
+        if what in utterance \
+            and meaning in utterance \
+            and (life in utterance \
+                or universe in utterance \
+                or everything in utterance):
+            self.speak('42')
+            return True # Indicate that the utterance was handled
         else:
-            self.speak_dialog('ppt.filenotopen')
+            return False
 
-    @intent_handler(IntentBuilder("PrevSlideIntent").require('PrevSlide'))
-    def handle_prev_slide(self, message):
-        if self.file_opened:
-            self.speak_dialog('ppt.prev')
-        else:
-            self.speak_dialog('ppt.filenotopen')
+    def shutdown(self):
+        """
+            Remove this skill from list of fallback skills.
+        """
+        self.remove_fallback(self.handle_fallback)
+        super(MeaningFallback, self).shutdown()
 
-    @intent_handler(IntentBuilder("ClosePPTIntent").require('ClosePPT'))
-    def handle_ppt_close(self, message):
-        if self.file_opened:
-            self.speak_dialog('ppt.close')
-        else:
-            self.speak_dialog('ppt.filenotopen')
 
-    def create_skill():
-        return PptControllerSkill()
-
+def create_skill():
+    return MeaningFallback()
