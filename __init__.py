230155a0b06a277c78f3024f3633d192b15ac8ce
asalekin <asalekin@gmail.com>
2018-08-03 14:21:53 -0400

    test dialog 1

--- __init__.py
+++ __init__.py
@@ -3,7 +3,73 @@ from mycroft.skills.core import MycroftSkill, intent_handler
 import nltk
 from nltk.stem.porter import *
 import json, socket
-from jsonsocket import Client
+
+class Client(object):
+
+    socket = None
+
+    def __del__(self):
+        self.close()
+
+    def connect(self, host, port):
+        self.socket = socket.socket()
+        self.socket.connect((host, port))
+        return self
+
+    def send(self, data):
+        if not self.socket:
+            raise Exception('You have to connect first before sending data')
+        _send(self.socket, data)
+        return self
+
+    def recv(self):
+        if not self.socket:
+            raise Exception('You have to connect first before receiving data')
+        return _recv(self.socket)
+
+    def recv_and_close(self):
+        data = self.recv()
+        self.close()
+        return data
+
+    def close(self):
+        if self.socket:
+            self.socket.close()
+            self.socket = None
+
+
+## helper functions ##
+
+def _send(socket, data):
+    try:
+        serialized = json.dumps(data)
+    except (TypeError, ValueError), e:
+        raise Exception('You can only send JSON-serializable data')
+    # send the length of the serialized data first
+    socket.send('%d\n' % len(serialized))
+    # send the serialized data
+    socket.sendall(serialized)
+
+def _recv(socket):
+    # read the length of the data, letter by letter until we reach EOL
+    length_str = ''
+    char = socket.recv(1)
+    while char != '\n':
+        length_str += char
+        char = socket.recv(1)
+    total = int(length_str)
+    # use a memoryview to receive the data chunk by chunk efficiently
+    view = memoryview(bytearray(total))
+    next_offset = 0
+    while total - next_offset > 0:
+        recv_size = socket.recv_into(view[next_offset:], total - next_offset)
+        next_offset += recv_size
+    try:
+        deserialized = json.loads(view.tobytes())
+    except (TypeError, ValueError), e:
+        raise Exception('Data received was not in JSON format')
+    return deserialized
+
 
 
 class MeaningFallback(FallbackSkill):
