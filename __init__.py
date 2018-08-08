0f3284065e9ec60af38f5eae4ab02d14638713ad
asalekin <asalekin@gmail.com>
2018-08-08 11:51:11 -0400

    test dialog 1

--- __init__.py
+++ __init__.py
@@ -478,10 +478,11 @@ class MeaningFallback(FallbackSkill):
 
  
             serialized=json.dumps({'task':TASK, 'nickname':Machine_NAME, 'type':Machine_Type, 'destination':LOCATION}).encode('utf-8')
-            self.speak("Performing the TASK "+TASK+ " by the Machine "+Machine_NAME+" to location "+str(LOCATION).strip('[]'))
+            self.speak("Performing the TASK "+TASK+ " by Machine Name "+Machine_NAME+" to location "+str(LOCATION).strip('[]'))
 
-
-            #clientsocket.connect(('10.12.100.22', 7423)) 
+            #serialized=json.dumps({'Task': 'move', 'Nickname':'shannon', 'Type':'3', 'Location':'area 3'}).encode('utf-8')
+            clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            #clientsocket.connect(('10.12.100.22', 7423))  #'localhost', 9099
             clientsocket.connect(('localhost', 9012))
             clientsocket.sendall(serialized)
             clientsocket.close()
